0033fca2950a94a2bb4c295777503660ff8e9ab6
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-21 21:30:19 -0300

    criando e executando Tasks

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -38,63 +38,30 @@ namespace ByteBank.View
         {
             var contas = r_Repositorio.GetContaClientes();
 
-            var contasQuantidadePorThread = contas.Count() / 4;
-
-            var contas_parte1 = contas.Take(contasQuantidadePorThread);
-            var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
-            var contas_parte3 = contas.Skip(contasQuantidadePorThread * 2).Take(contasQuantidadePorThread);
-            var contas_parte4 = contas.Skip(contasQuantidadePorThread * 3);
-
             var resultado = new List<string>();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            Thread thread_parte1 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte1)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte2 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte2)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte3 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte3)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte4 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte4)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
+            //define qual tarefa será executada ou não : TaskScheduler
 
-            thread_parte1.Start();
-            thread_parte2.Start();
-            thread_parte3.Start();
-            thread_parte4.Start();
-
-            while (thread_parte1.IsAlive || thread_parte2.IsAlive
-                || thread_parte3.IsAlive || thread_parte4.IsAlive)
+            // o linq só executada o que será utilizado, então devemos forçar essa inicialização 
+            //aqui utilizamos o ToArray()
+            var contasTarefas = contas.Select(conta =>
             {
-                Thread.Sleep(250);
-                //Não vou fazer nada
-            }
+                //constroi tarefas  : Factory
+                return Task.Factory.StartNew(() =>
+                {
+                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoConta);
+                });
+            }).ToArray();
+
+            // faz a verificação se a thread atual está sendo executada, mesma função do 
+            //IsAlive
+
+            Task.WaitAll(contasTarefas);
 
             var fim = DateTime.Now;
 
